851d0b9f47e79013b443dfaddea446a953f72e46
Ogunkanmi Ayodele <ayo4oyo@gmail.com>
2019-03-28 18:35:43 +0100

    Add files via upload

---
+++ day-at-the-supermarket.py
@@ -0,0 +1,28 @@
+#Let's manage our own supermarket and buy some goods along the way!
+
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
+    
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+  total = 0
+  for each in food:
+    if stock[each] > 0:
+      total += prices[each]
+      stock[each] -= 1
+  return total
+  print total
+  
